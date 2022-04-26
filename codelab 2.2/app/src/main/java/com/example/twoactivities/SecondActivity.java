6ad4c0e5798c781ec743ce9aca28338d0b980279
Your Namephamvanlinhxyz <linh.pv194094@sis.hust.edu.vn>
2022-04-26 16:25:09 +0700

    add codelab 2.2

---
+++ codelab 2.2/app/src/main/java/com/example/twoactivities/SecondActivity.java
@@ -0,0 +1,77 @@
+package com.example.twoactivities;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.PersistableBundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.TextView;
+
+public class SecondActivity extends AppCompatActivity {
+    private static final String LOG_TAG = SecondActivity.class.getSimpleName();
+    public static final String EXTRA_REPLY = "com.example.android.twoactivities.extra.REPLY";
+    private EditText mReply;
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        Log.d(LOG_TAG, "onStart");
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        Log.d(LOG_TAG, "onResume");
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        Log.d(LOG_TAG, "onStop");
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        Log.d(LOG_TAG, "onDestroy");
+    }
+
+    @Override
+    protected void onRestart() {
+        super.onRestart();
+        Log.d(LOG_TAG, "onRestart");
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        Log.d(LOG_TAG, "onPause");
+    }
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_second);
+
+        Intent intent = getIntent();
+        String message = intent.getStringExtra(MainActivity.EXTRA_MESSAGE);
+        TextView textView = findViewById(R.id.text_message);
+        textView.setText(message);
+
+        mReply = findViewById(R.id.editText_second);
+    }
+
+    public void returnReply(View v) {
+        String reply = mReply.getText().toString();
+        Intent intent = new Intent(this, MainActivity.class);
+        intent.putExtra(EXTRA_REPLY, reply);
+        setResult(RESULT_OK, intent);
+        Log.d(LOG_TAG, "End SecondActivity");
+        finish();
+        Log.d(LOG_TAG, "Reply Message!");
+    }
+}
